a725ff39bd956999f923659d86d00c292b626618
David Kybelka <dvdkblk@gmail.com>
2024-03-18 10:27:33 +0100

    add geodesic to functions

--- functions.r
+++ functions.r
@@ -22,3 +22,26 @@ siena07ToConvergence <- function(alg, dat, eff, ans0 = NULL, max_runs = 30, set,
     ans
 }
 
+# GeodesicDistribution calculates the distribution of non-directed
+# geodesic distances; see ?sna::geodist
+# The default for levls reflects the usual phenomenon
+# that geodesic distances larger than 5
+# do not differ appreciably with respect to interpretation.
+# Note that the levels of the result are named;
+# these names are used in the \code{plot} method.
+GeodesicDistribution <- function (i, data, sims, period, groupName,
+                                  varName, levls=c(1:5,Inf), cumulative=TRUE, ...) {
+  x <- networkExtraction(i, data, sims, period, groupName, varName)
+  require(sna)
+  a <- sna::geodist(symmetrize(x))$gdist
+  if (cumulative)
+  {
+    gdi <- sapply(levls, function(i){ sum(a<=i) })
+  }
+  else
+  {
+    gdi <- sapply(levls, function(i){ sum(a==i) })
+  }
+  names(gdi) <- as.character(levls)
+  gdi
+}
